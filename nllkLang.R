634be17aa88be6ac383164a0879439e97209d909
gloaguen <pierre.gloaguen@gmail.com>
2018-03-02 18:35:46 +0100

    Addition of the ozaki method. PLUS addition of a Langevin factor. It seems to me that 0.5 was forgotten

--- nllkLang.R
+++ nllkLang.R
@@ -1,4 +1,4 @@
-
+source("OzakiFunctions.R")
 #' Negative log-likelihood function for Langevin movement model
 #' 
 #' At the moment, this function is based on the Euler approximation.
@@ -10,9 +10,28 @@
 #' the columns are the dimensions (x and y), and the layers index the covariates.
 #' 
 #' @return Negative log-likelihood
-nllkLang <- function(beta,xy,time,gradarray) {
+nllkLang <- function(beta, xy, time, gradarray, jacobarray = NULL, method = "euler") {
     dt <- diff(time)
-    gradmat <- apply(gradarray, 2, function(mat) mat%*%beta)
-    llk <- sum(dnorm(xy[-1,],xy[-nrow(xy),]+dt*gradmat[-nrow(xy),],sqrt(dt),log=TRUE))
+    LangevinFactor <- 0.5# 0.5 to fit with the Langevin 0.5 factor
+    gradmat <- LangevinFactor * apply(gradarray, 2, function(mat) mat %*% beta)
+    n <- nrow(xy)
+    if(method == "euler"){
+      llk <- sum(dnorm(xy[-1, ], xy[-n,] + dt * gradmat[-n, ], sqrt(dt), log=TRUE))
+    }
+    if(method == "ozaki"){
+      jacobmat <- LangevinFactor * apply(jacobarray, 2, function(mat) mat %*% beta)
+      Means <- getOzakiMean(xy, dt, gradmat, jacobmat)#Matrix of size (n-1) * 2
+      CovsInv <- getOzakiCovariance(xy, dt, gradmat, jacobmat, Inv = T)#Matrix of size (n-1) * 4
+      DetsInv <- apply(CovsInv, 1, vecDetM)# computing determinant to spot potential problems
+      ToEuler <- NumProblemDet(DetsInv)# Potential problematic segments will be dealt using Euler approximation
+      if(any(ToEuler)){
+        Nr <- sum(ToEuler)
+        Means[ToEuler, ] <- xy[-n,][ToEuler,] + dt[ToEuler] * (gradmat[-n,][ToEuler, ])
+        CovsInv[ToEuler, ] <- cbind(1/ dt[ToEuler], rep(0, Nr), 
+                                    rep(0, Nr), 1 / dt[ToEuler]) 
+        DetsInv[ToEuler] <- 1 / (dt[ToEuler] * dt[ToEuler])
+      }
+      llk <- sum(logdnorm2d(xs = xy[-1, ], vecMeans = Means, vecCovInvs = CovsInv, DetInv = DetsInv))
+    }
     return(-llk)
 }
