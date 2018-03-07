4865bab6577b287ee21773fce5bb802808666951
TheoMichelot <theo.michelot@gmail.com>
2018-03-07 14:51:17 +0000

    update doc LinearAlgebraFunctions OzakiFunctions

--- LinearAlgebraFunctions.R
+++ LinearAlgebraFunctions.R
@@ -1,95 +1,102 @@
-#' Functions for basic algebra when a 4-vector codes a 2 by 2 matrix
-#' 
-#' @param vec1,vec2,vecM vectors of length 4 vectors, for matrix M 2 by 2, vectors are coded
-#' as expected by R. I.E, a matrix 
-#' a11 a12
-#' a21 a22 is coded as the vector (a11, a21, a12, a22)
-#' @param vec2D a vector of length 2
-#' @return vectors of length 4 corresponing to the linear algebra computation, 
-#' except for the MksM and MksM that returns the matrix correspnding to the inner kronecker sum
-#' and its inverse respectively
+
+# Functions for basic algebra when a 4-vector codes a 2 by 2 matrix
+
+# Matrix multiplication
 vecMtimesM <- function(vec1, vec2){
-  return(c(vec1[1] * vec2[1] + vec1[3] * vec2[2],
-           vec1[2] * vec2[1] + vec1[4] * vec2[2],
-           vec1[1] * vec2[3] + vec1[3] * vec2[4],
-           vec1[2] * vec2[3] + vec1[4] * vec2[4]))
+    return(c(vec1[1] * vec2[1] + vec1[3] * vec2[2],
+             vec1[2] * vec2[1] + vec1[4] * vec2[2],
+             vec1[1] * vec2[3] + vec1[3] * vec2[4],
+             vec1[2] * vec2[3] + vec1[4] * vec2[4]))
 }
 
+# Determinant
 vecDetM <- function(vecM){
-  vecM[1] * vecM[4] - vecM[2] * vecM[3]
+    vecM[1] * vecM[4] - vecM[2] * vecM[3]
 }
 
+# Inverse
 vecSolveM <- function(vecM){
-  Det <- vecM[1] * vecM[4] - vecM[2] * vecM[3]#determinant
-  if(is.na(Det) | Det == 0)
-    return(rep(NA, 4))
-  return(c(vecM[4], -vecM[2], -vecM[3], vecM[1]) / Det)
+    Det <- vecM[1] * vecM[4] - vecM[2] * vecM[3] # determinant
+    if(is.na(Det) | Det == 0)
+        return(rep(NA, 4))
+    return(c(vecM[4], -vecM[2], -vecM[3], vecM[1]) / Det)
 }
 
+# Matrix-vector multiplication
 vecMtimesV <- function(vecM, vec2D){
-  return(c(vecM[1] * vec2D[1] + vecM[3] * vec2D[2],
-           vecM[2] * vec2D[1] + vecM[4] * vec2D[2]))
-}
-vecExpM <- function(vecM){
-  return(as.numeric(expm::expm(matrix(vecM, nrow = 2))))
-}
-MksumM <- function(vecM){# Inner Kronecker sum
-  a <- vecM[1]; b <- vecM[2]; c <- vecM[3]; d <- vecM[4]
-  VecMat <- c(2 * a, c    , c    , 0,
-              b    , a + d, 0    , c,
-              b    , 0    , a + d, c,
-              0    , b    , b    , 2 * d )
-  matrix(VecMat, nrow = 4, ncol = 4, byrow = T)
+    return(c(vecM[1] * vec2D[1] + vecM[3] * vec2D[2],
+             vecM[2] * vec2D[1] + vecM[4] * vec2D[2]))
 }
 
-MksumMInv <- function(vecM){# Inverse of the inner Kronecker sum
-  a <- vecM[1]; b <- vecM[2]; c <- vecM[3]; d <- vecM[4]
-  Det <- 2 * (a + d) * (a*d - b*c)
-  if(Det == 0){
-    return(matrix(NA, nrow = 4, ncol = 4))
-  }
-  VecMat <- c(d*d + a*d - b*c, -c*d       , -c*d       , c*c          ,
-              -b*d           , 2*a*d - b*c, b*c        , -a*c         ,
-              -b*d           , b*c        , 2*a*d - b*c, -a*c         ,
-              b*b            , -a*b       , -a*b       ,a*a + d*a - b*c)
-  matrix(VecMat / Det, nrow = 4, ncol = 4, byrow = T)
+# Matrix exponential
+vecExpM <- function(vecM){
+    return(as.numeric(expm::expm(matrix(vecM, nrow = 2))))
+}
+
+# Inner Kronecker sum (output is a matrix)
+MksumM <- function(vecM){
+    a <- vecM[1]; b <- vecM[2]; c <- vecM[3]; d <- vecM[4]
+    VecMat <- c(2 * a, c    , c    , 0,
+                b    , a + d, 0    , c,
+                b    , 0    , a + d, c,
+                0    , b    , b    , 2 * d )
+    matrix(VecMat, nrow = 4, ncol = 4, byrow = T)
+}
+
+# Inverse of inner Kronecker sum (output is a matrix)
+MksumMInv <- function(vecM){
+    a <- vecM[1]; b <- vecM[2]; c <- vecM[3]; d <- vecM[4]
+    Det <- 2 * (a + d) * (a*d - b*c)
+    if(Det == 0){
+        return(matrix(NA, nrow = 4, ncol = 4))
+    }
+    VecMat <- c(d*d + a*d - b*c, -c*d       , -c*d       , c*c          ,
+                -b*d           , 2*a*d - b*c, b*c        , -a*c         ,
+                -b*d           , b*c        , 2*a*d - b*c, -a*c         ,
+                b*b            , -a*b       , -a*b       ,a*a + d*a - b*c)
+    matrix(VecMat / Det, nrow = 4, ncol = 4, byrow = T)
 }
 
 #' Fast function for computing bivariate quadratic form
 #' 
-#' @param vec2ds matrix with 2 columns and, say, n lines
-#' as 
-#' @param vecMs  matrix with 4 columns and n line(same number of lines as x)
-#' @return For each row v of vecDs and M of vecMs, return t(v) times matrix(M) times v
-vecVMV <- function(vec2Ds, vecMs){
-  return(vecMs[, 1] * vec2Ds[, 1] * vec2Ds[, 1] 
-         + vec2Ds[, 1] * vec2Ds[, 2] * (vecMs[, 2] + vecMs[, 3])
-         + vecMs[ ,4] * vec2Ds[, 2] * vec2Ds[, 2])
+#' Calculates t(v) %*% M %*% v for multiple input vectors v and matrices M.
+#' 
+#' @param allv Matrix with two columns. Each row corresponds to a 2-vector v.
+#' @param allM Matrix with 4 columns, and as many rows as allv. Each row corresponds
+#' to a 2x2 matrix M (stored as a 4-vector).
+#' 
+#' @return Vector of length the number of rows of the input matrices.
+#' For each row v of allv and M of allM, returns t(v) times matrix(M) times v.
+vecVMV <- function(allv, allM){
+    return(allM[, 1] * allv[, 1] * allv[, 1] 
+           + allv[, 1] * allv[, 2] * (allM[, 2] + allM[, 3])
+           + allM[ ,4] * allv[, 2] * allv[, 2])
 }
 
 #' Fast function for log density of a bivariate normal distribution
 #' 
-#' @param xs matrix with 2 columns and, say, n lines
-#' as 
-#' @param vecMeans  matrix of same dimension as x, corresponding to evaluated means
-#' @param vecCovInvs matrix of n lines and 4 columns, vectorization of the PRECISION matrix 
+#' @param xs Matrix with 2 columns and, say, n lines
+#' @param vecMeans Matrix of same dimension as xs, corresponding to evaluated means
+#' @param vecCovInvs Matrix of n lines and 4 columns, vectorization of the PRECISION matrix 
 #' (inverse of covariance)
-#' @param DetInv number or vector of length n giving the determinant of each matrix of vecCovInvs,
-#' can be omitted, it will be computed in this case
+#' @param DetInv Number or vector of length n giving the determinant of each matrix of vecCovInvs.
+#' If NULL (default), it will be computed.
+#' 
 #' @return For each row x xs, m of vecMeans, C of vecCovInvs, compute the pdf (evaluated in x) of a 
 #' bivariate normal N(m, C^(-1))
 logdnorm2d <- function(xs, vecMeans, vecCovInvs, DetInv = NULL){
-  if(is.null(DetInv)){
-    DetInv <- apply(vecCovInvs, 1, vecDetM)
-  }
-  - log(2 * pi) + 0.5 * log(DetInv) - 0.5 * vecVMV(xs - vecMeans, vecCovInvs)
+    if(is.null(DetInv)){
+        DetInv <- apply(vecCovInvs, 1, vecDetM)
+    }
+    return(- log(2 * pi) + 0.5 * log(DetInv) - 0.5 * vecVMV(xs - vecMeans, vecCovInvs))
 }
 
 
 #' Spot abnormal values of determinants to avoid numerical problems in the Ozaki method
 #' 
 #' @param Dets vector of determinants
-#' @return A vector of booleans. False is the determinant is acceptable, True if is problematic
+#' 
+#' @return A vector of booleans. FALSE if the determinant is acceptable, TRUE otherwise.
 NumProblemDet <- function(Dets){
-  sapply(Dets, function(x) isTRUE(x < 10^(-5) | x > 10^(10)))
+    sapply(Dets, function(x) isTRUE(x < 10^(-5) | x > 10^(10)))
 }
--- OzakiFunctions.R
+++ OzakiFunctions.R
@@ -1,60 +1,68 @@
 source("LinearAlgebraFunctions.R")
-#' Obtaining Ozaki required mean
-#' 
-#' At the moment, this function is based on the Euler approximation.
+#' Mean of Ozaki transition density
 #' 
 #' @param xy Matrix of observed locations (two columns: x, y)
-#' @param time Vector of times of observations
+#' @param Deltas Vector of time intervals
 #' @param gradmat Computed gradient at xy positions. (In the case of the RSF, it
 #' is already the sum of all gradients). It is a T by 2 matrix where T is the number 
 #' of observations for the given trajectory
 #' @param jacobmat Computed Jacobian at xy positions. It must be T by 4 matrix
 #' in the following order (d/dxdx, d/dxdy, d/dydx, d/dydy)
-#' @return A matrix having size of xy, corresponding of the expected mean 
-#' of the ozaki linearization, which is the muO of the article of Gloaguen et al
+#' 
+#' @return A matrix having size of xy, corresponding to the expected mean 
+#' of the Ozaki linearization, denoted muO in Gloaguen et al.
 #' 
 getOzakiMean <- function(xy, Deltas, gradmat, jacobmat, jacobmatInv) {
-  jacobmatInv <- t(apply(jacobmat, 1, vecSolveM))  #For each row, computing the inverse row
-  Idvec <- c(1, 0, 0, 1)#I2 in vectorized version
-  return(t(sapply(1:(nrow(xy) - 1), function(i){
-    if(any(is.na(jacobmatInv[i, ]))){# If numerical problem, returns the euler approx
-      return(xy[i, ] + Deltas[i] * gradmat[i, ])
-    }
-    else{
-      expJiMinusId <- (vecExpM(jacobmat[i, ] * Deltas[i]) - Idvec)
-      return(xy[i, ] + vecMtimesV(expJiMinusId, vecMtimesV(jacobmatInv[i, ], gradmat[i, ])))
-    }
-  })))
+    jacobmatInv <- t(apply(jacobmat, 1, vecSolveM))  # For each row, compute the inverse row
+    Idvec <- c(1, 0, 0, 1) # I2 in vectorized version
+    return(t(sapply(1:(nrow(xy) - 1), function(i){
+        if(any(is.na(jacobmatInv[i, ]))){ # If numerical problem, returns the euler approx
+            return(xy[i, ] + Deltas[i] * gradmat[i, ])
+        }
+        else{
+            expJiMinusId <- (vecExpM(jacobmat[i, ] * Deltas[i]) - Idvec)
+            return(xy[i, ] + vecMtimesV(expJiMinusId, vecMtimesV(jacobmatInv[i, ], gradmat[i, ])))
+        }
+    })))
 }
 
+#' Covariance matrix of Ozaki transition density
+#' 
+#' @param xy Matrix of observed locations (two columns: x, y)
+#' @param Deltas Vector of time intervals
+#' @param gradmat Computed gradient at xy positions. (In the case of the RSF, it
+#' is already the sum of all gradients). It is a T by 2 matrix where T is the number 
+#' of observations for the given trajectory
+#' @param jacobmat Computed Jacobian at xy positions. It must be T by 4 matrix
+#' in the following order (d/dxdx, d/dxdy, d/dydx, d/dydy)
 getOzakiCovariance <- function(xy, Deltas, gradmat, jacobmat, Inv = F) {
-  Idvec <- c(1, 0, 0, 1)
-  I4 <- diag(1, 4)
-  return(t(sapply(1:(nrow(xy) - 1), function(i){
-    if(all.equal(jacobmat[i, 1], 0) == T | all.equal(jacobmat[i, 4], 0) == T)# All betas equal to 0
-      vecCov <- c(Deltas[i], 0, 0, Deltas[i])#Euler approximation
-    else{
-      JksJ <- MksumM(jacobmat[i, ])# Kronecker sum
-      JksJinv <- MksumMInv(jacobmat[i, ])# Inverse Kronecker sum
-      if(any(is.na(JksJinv))){#Case of numerical problem
-        vecCov <- c(Deltas[i], 0, 0, Deltas[i])#Euler approximation
-      }
-      else{
-        vecCov <- as.numeric(JksJinv %*% (expm::expm(JksJ * Deltas[i]) - I4) %*% Idvec)
-        if(mode(all.equal(vecCov[2], vecCov[3], tolerance = 10^(-4))) == "character"){
-          print("Line", i)
-          print("jacobmat[i,]")
-          print(jacobmat[i, ])
-          print("Non symmetric matrix")
+    Idvec <- c(1, 0, 0, 1)
+    I4 <- diag(1, 4)
+    return(t(sapply(1:(nrow(xy) - 1), function(i){
+        if(all.equal(jacobmat[i, 1], 0) == T | all.equal(jacobmat[i, 4], 0) == T) # All betas equal to 0
+            vecCov <- c(Deltas[i], 0, 0, Deltas[i]) # Euler approximation
+        else{
+            JksJ <- MksumM(jacobmat[i, ]) # Kronecker sum
+            JksJinv <- MksumMInv(jacobmat[i, ]) # Inverse Kronecker sum
+            if(any(is.na(JksJinv))){ # Case of numerical problem
+                vecCov <- c(Deltas[i], 0, 0, Deltas[i]) # Euler approximation
+            }
+            else{
+                vecCov <- as.numeric(JksJinv %*% (expm::expm(JksJ * Deltas[i]) - I4) %*% Idvec)
+                if(mode(all.equal(vecCov[2], vecCov[3], tolerance = 10^(-4))) == "character"){
+                    print("Line", i)
+                    print("jacobmat[i,]")
+                    print(jacobmat[i, ])
+                    print("Non symmetric matrix")
+                }
+            }
         }
-      }
-    }
-    if(Inv){
-      return(vecSolveM(vecCov))# directly to inverse
-    }
-    else{
-      return(vecCov)
-    }
-  })))
+        if(Inv){
+            return(vecSolveM(vecCov)) # directly to inverse
+        }
+        else{
+            return(vecCov)
+        }
+    })))
 }
 
