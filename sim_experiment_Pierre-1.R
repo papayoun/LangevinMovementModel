415af18cb1868cd6e6f50bf2acff6a2adcaf86bf
TheoMichelot <theo.michelot@gmail.com>
2018-04-20 13:25:37 +0100

    tidy up simulation experiment

--- sim_experiment_Pierre-1.R
+++ sim_experiment_Pierre-1.R
@@ -1,14 +1,4 @@
 
-#' Ce script génère deux covariables aléatoires, et calcule la distance au centre (au carré)
-#' comme autre covariable. Tu peux les visualiser avec plot(covlist[[i]]) (pour i=1,2,3).
-#' Ensuite, la distribution d'utilisation est définie par une RSF, i.e. exp(beta%*%covariables),
-#' où je choisis beta = (2,4,-6). Tu peux la visualiser avec plot(rsfRaster).
-#' Je simule 1000 positions avec un schéma d'Euler, sur cette distribution d'utilisation. Finalement,
-#' j'utilise la pseudo-vraisemblance basée sur le schéma d'Euler, au même pas de temps que celui de
-#' la simulation, pour estimer les paramètres du modèle. Donc, en l'occurrence, j'utilise la "vraie"
-#' vraisemblance du processus générateur, ce qui devrait nous permettre de retrouver les paramètres
-#' sans problème. Pourtant, c'est pas vraiment le cas.
-
 # Load packages
 library(raster)
 library(numDeriv)
@@ -20,6 +10,7 @@ source("RSF.R")
 source("utility.R")
 source("simLang.R")
 source("nllkLang.R")
+source("eulerLSE.R")
 
 ###############################
 ## Define covariates and RSF ##
@@ -68,30 +59,48 @@ ntrack <- 2
 dt <- 1
 Tmax <- 500
 time <- seq(0,Tmax,by=dt)
+data <- NULL
+
 set.seed(1)
-xy <- NULL
-ID <- rep(1:ntrack,each=Tmax/dt+1)
 for(zoo in 1:ntrack) {
     track <- simLang(beta=beta, time=time, xy0=runif(2,-10,10), 
                      xgrid=xgrid, ygrid=ygrid, covarray=covarray)
-    xy <- rbind(xy, track)
+    data <- rbind(data, cbind(rep(zoo,nrow(track)),track,time))
 }
-time <- rep(time,ntrack)
+colnames(data) <- c("ID","x","y","time")
+
+# # save full simulated data set before thinning
+# alldata <- data
+# # thin
+# nobs <- nrow(alldata)
+# ind <- sort(sample(1:nrow(alldata),size=nobs,replace=FALSE))
+# data <- alldata[ind,]
+
+ID <- data[,"ID"]
+xy <- data[,c("x","y")]
+time <- data[,"time"]
 
 plot(log(rsfRaster))
 points(xy,type="o",cex=0.4)
 
-###########################
-## Fit with Euler method ##
-###########################
 # Evaluate covariate gradients at observed locations
 gradarray <- covGrad(xy, xgrid, ygrid, covarray)
-# Optimise log-likelihood
+
+######################################
+## Fit UD with Euler discretization ##
+######################################
+# Via numerical MLE
 fit <- nlminb(start = beta, objective = nllkLang, xy = xy, time = time, ID=ID,
               gradarray = gradarray, control = list(trace=1))
 
+# Via least squares
+lse <- eulerLSE(ID=ID, time=time, xy=xy, gradarray=gradarray)
+
+# 95% CI
+cbind(lse$Bhat-1.96*sqrt(diag(lse$var)),lse$Bhat,lse$Bhat+1.96*sqrt(diag(lse$var)))
+
 # Compute estimated RSF
-betaMLE <- fit$par
+betaMLE <- lse$Bhat
 rsfRasterMLE <- 0
 for(i in 1:length(covlist))
     rsfRasterMLE <- rsfRasterMLE + betaMLE[i]*covlist[[i]]
@@ -102,29 +111,3 @@ rsfRasterMLE <- exp(rsfRasterMLE)
 plot(values(rsfRaster)/sum(values(rsfRaster)),
      values(rsfRasterMLE)/sum(values(rsfRasterMLE)))
 abline(0,1,col=2)
-
-
-#############################################
-## Fit with Euler method using lm approach ##
-#############################################
-d <- dim(gradarray)[3]
-n <- dim(gradarray)[1]
-
-i0 <- which(ID[-1]!=ID[-n])
-i2 <- c(i0, n) # last obs of each track
-
-##  design matrix construction
-X <- 0.5*rbind(gradarray[-i2,1,], gradarray[-i2,2,])
-TT <- diag(rep(diff(time)[-i0],2))
-
-TZ <- matrix(apply(xy, 2, function(c) diff(c)[-i0]), ncol=1)
-
-## estimation
-XTTX    <- t(X) %*% TT %*% X
-XTTXinv <- solve(XTTX)
-(Bhat  <- XTTXinv %*% t(X) %*% TZ)
-
-diag(XTTXinv)
-
-# 95% CI
-cbind(Bhat-1.96*sqrt(diag(XTTXinv)),Bhat,Bhat+1.96*sqrt(diag(XTTXinv)))
