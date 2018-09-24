d10a30fff65a045ac63b20b098c009fcdda9f6ca
TheoMichelot <theo.michelot@gmail.com>
2018-09-24 15:10:14 +0100

    fit CTCRW to tracks in loop

--- sealion_analysis/wilsonEtAl_with_pred.R
+++ sealion_analysis/wilsonEtAl_with_pred.R
@@ -1,12 +1,11 @@
 
-## The code below is taken from:
+## The code below is adapted from:
 ## Wilson, K., Hanks, E., & Johnson, D. (2018). 
 ## Estimating animal utilization densities using continuous‐time Markov chain models.
 ## Methods in Ecology and Evolution, 9(5), 1232-1240.
 ## (github.com/kenady/ctmcUD-MEE)
 
 library(crawl) # devtools::install_github("NMML/crawl") for latest version
-library(ctmcmove)
 library(gdistance)
 library(lubridate)
 library(raster)
@@ -29,57 +28,64 @@ dat_toproj <- spTransform(dat_toproj, CRS("+init=epsg:3338")) %>%
   dplyr::select(Deploy_ID, GMT, x, y)
 ssl_data = full_join(ssl_data, dat_toproj, by=c("Deploy_ID", "GMT"))
 
-###################################
-# Fit CTMC UD to first animal    ##
-###################################
-# temp_dat <- dplyr::filter(ssl_data, Deploy_ID==14809) %>% arrange(GMT) #pull out one sea lion
-temp_dat <- ssl_data %>% arrange(GMT)
+##################################
+# Fit CTMC UD to all animals    ##
+##################################
+allpred <- NULL
 
-#set Argos error & add a constraint parameter for estimating Argos error
-# in CTCRW imputation model
-temp_dat$argos_class = factor(temp_dat$argos_class, levels=c("3","2","1","0","A","B"))
+for(id in unique(ssl_data$Deploy_ID)) {
+    cat("\nProcessing individual",id,"...\n")
+    temp_dat <- dplyr::filter(ssl_data, Deploy_ID==id) %>% arrange(GMT) #pull out one sea lion
+    
+    #set Argos error & add a constraint parameter for estimating Argos error
+    # in CTCRW imputation model
+    temp_dat$argos_class = factor(temp_dat$argos_class, levels=c("3","2","1","0","A","B"))
+    
+    # Define 'haul' variable for determining when the animal is hauled out. These will
+    # be removed in the CTMC analysis
+    temp_dat %>%  mutate(
+        haul = (
+            DryTime==1 & c(0,diff(DryTime==1)==0) |
+                DryTime==0 & c(0,diff(DryTime==1)==-1)
+        )
+    ) -> temp_dat
+    
+    ## Initial state values from CTCRW model in crawl (for imputation of raw telemetry)
+    initial <- list(a=c(temp_dat$x[1],0, temp_dat$y[1],0),
+                    P=diag(c(10000^2,5400^2,10000^2,5400^2)))
+    # Fixed parameter values for CTCRW imputation model
+    fixPar = c(log(250), log(500), log(1500), rep(NA,5), 0) 
+    # Lower bounds for parameter estimates
+    constr = list(lower=c(rep(log(1500),3),rep(-Inf,2)), upper=rep(Inf,5))
+    # Check that parameters are as expected
+    crawl::displayPar( mov.model=~1, err.model=list(x=~argos_class-1),data=temp_dat,
+                       activity=~I(1-DryTime),fixPar=fixPar) #with dry time
+    
+    # 2) Run crawl model (Fit CTCRW model)
+    # --------------------------------------------------------------------
+    set.seed(123)
+    mi_fit <- crawl::crwMLE(
+        mov.model=~1, err.model=list(x=~argos_class-1), activity=~I(1-DryTime),
+        data=temp_dat, coord=c("x","y"), Time.name="GMT",
+        initial.state=initial, fixPar=fixPar,
+        constr=constr, #prior=ln.prior,
+        method="L-BFGS-B",
+        control=list(maxit=2000, trace=1, REPORT=10),
+        initialSANN=list(maxit=250, temp=10, trace=1, REPORT=10))
+    
+    # 3) Predict locations at times of observations
+    # --------------------------------------------------------------------
+    predloc <- crwPredict(mi_fit)
+    whichObs <- which(!is.na(temp_dat$longitude))
+    predloc <- predloc[whichObs,]
+    
+    pred <- data.frame(ID = predloc$Deploy_ID, 
+                          time = temp_dat$GMT[whichObs],
+                          x = predloc$mu.x,
+                          y = predloc$mu.y)
+    
+    allpred <- rbind(allpred, pred)
+}
 
-# Define 'haul' variable for determining when the animal is hauled out. These will
-# be removed in the CTMC analysis
-temp_dat %>%  mutate(
-                  haul = (
-                    DryTime==1 & c(0,diff(DryTime==1)==0) |
-                      DryTime==0 & c(0,diff(DryTime==1)==-1)
-                    )
-                  ) -> temp_dat
-
-## Initial state values from CTCRW model in crawl (for imputation of raw telemetry)
-initial <- list(a=c(temp_dat$x[1],0, temp_dat$y[1],0),
-                P=diag(c(10000^2,5400^2,10000^2,5400^2)))
-# Fixed parameter values for CTCRW imputation model
-fixPar = c(log(250), log(500), log(1500), rep(NA,5), 0) 
-# Lower bounds for parameter estimates
-constr = list(lower=c(rep(log(1500),3),rep(-Inf,2)), upper=rep(Inf,5))
-# Check that parameters are as expected
-crawl::displayPar( mov.model=~1, err.model=list(x=~argos_class-1),data=temp_dat,
-            activity=~I(1-DryTime),fixPar=fixPar) #with dry time
-
-# 2) Run crawl model (Fit CTCRW model)
-# --------------------------------------------------------------------
-set.seed(123)
-mi_fit <- crawl::crwMLE(
-  mov.model=~1, err.model=list(x=~argos_class-1), activity=~I(1-DryTime),
-  data=temp_dat, coord=c("x","y"), Time.name="GMT",
-  initial.state=initial, fixPar=fixPar,
-  constr=constr, #prior=ln.prior,
-  method="L-BFGS-B",
-  control=list(maxit=2000, trace=1, REPORT=10),
-  initialSANN=list(maxit=250, temp=10, trace=1, REPORT=10))
-
-# 3) Predict locations at times of observations
-# --------------------------------------------------------------------
-predloc <- crwPredict(mi_fit)
-whichObs <- which(!is.na(temp_dat$longitude))
-predloc <- predloc[whichObs,]
-
-preddat <- data.frame(ID = predloc$Deploy_ID, 
-                      time = temp_dat$GMT[whichObs],
-                      x = predloc$mu.x,
-                      y = predloc$mu.y)
-
-# write.csv(preddat, file="~/git/LangevinMovementModel/sealion_analysis/SSLpreddat.csv", row.names=FALSE)
+write.csv(allpred, file="~/git/LangevinMovementModel/sealion_analysis/SSLpreddat.csv",
+          row.names=FALSE)
